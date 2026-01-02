f2a4733f10f5b13a24bb419ff5e610613fd824b1
Pratik Sugandhi <pratiksugandhi4@gmail.com>
2026-01-02 16:56:58 +0530

    Added the use of Navigate hooks

--- src/Pages/StartGame/StartGame.jsx
+++ src/Pages/StartGame/StartGame.jsx
@@ -1,12 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import TextInputFormContainer from "../../Components/TextInputForm/TextInputFormContainer";
 function StartGame()
 {
+    const navigate=useNavigate();
+    function handleSubmit()
+    {
+        navigate('/play');
+    }
     return(
         <>
             <h1>StartGame Page</h1>
             <br />
             <Link to='/play'>Play Game</Link>
             <Link to='/App'>Go to App</Link>
+            <TextInputFormContainer onSubmit={handleSubmit} />
         </>
     )
 }
